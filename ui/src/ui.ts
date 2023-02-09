b82c300e6fb9ee44774ee0c0fadeead28f8e7e03
zhufeng <zhfjyq@163.com>
2023-02-09 10:04:21 +0800

    OK

--- ui/src/ui.ts
+++
@@ -1,9 +0,0 @@
-import { Scope } from 'wcex';
-import { popOptions } from './pop1';
-
-
-export default class extends Scope {
-    onReady(): void {
-        popOptions.globalEl = this.$id.pop;
-    }
-}
