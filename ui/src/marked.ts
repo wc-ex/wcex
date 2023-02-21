0caf20040d8980e8a1a78293037ec3f806e44e73
zhufeng <zhfjyq@163.com>
2023-02-21 16:24:45 +0800

    OK

---
+++ ui/src/marked.ts
@@ -0,0 +1,25 @@
+import { marked } from 'marked';
+// import hljs  from 'highlight.js';
+import { Scope } from 'wcex';
+
+export default class extends Scope {
+  src=""
+  hljs=''
+  onReady(): void {
+    this.$log('---',this.hljs )
+    this.$watch(()=>this.src,()=>{
+      this.updateMarked()
+    })
+    this.updateMarked()
+  }
+  async updateMarked() {
+    if(this.src){
+      let text = await this.$loader.getFile(this.src).getResult()
+      // let text = await (await fetch()).text()
+      let html = marked.parse(text.replace(/\\n/g, '\n'));
+      this.$id.mark.innerHTML = html;  
+    }else{
+      this.$id.mark.innerHTML=""
+    }
+  }
+}
