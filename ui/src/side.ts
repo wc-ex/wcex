9bf58d4d840809bc4932b1e196368959b873c21a
zhufeng <zhfjyq@163.com>
2023-03-06 16:39:08 +0800

    OK

--- ui/src/side.ts
+++ ui/src/side.ts
@@ -7,19 +7,21 @@ interface ISideOptions {
   // 属性
   attrs?: { [k: string]: string };
   //弹出位置
-  pos: "t" | "b" | "l" | "r" ;
-  // 边缘框大小
-  size:string;
+  pos?: "t" | "b" | "l" | "r";
+  // 框大小
+  size?: string;
+  autoclose?:boolean;
 }
 
 let popInstance = undefined as Side | undefined;
 
 WC.usePlugins({
   name: "$side",
+  
   scope: {
     $side(tagOrEl: string | HTMLElement, options?: ISideOptions) {
-        popInstance?.open(tagOrEl, options);
-      },
+      popInstance?.open(tagOrEl, options);
+    },
   },
 });
 
@@ -27,9 +29,10 @@ export default class Side extends Scope {
   el = undefined as HTMLElement | undefined;
   show = false;
   uiRootEl = undefined as HTMLElement | undefined;
-  size='12em';
-  pos='r';
-  sty={} as {color?:any}
+  size = "12em";
+  pos = "r";
+  sty = {} as { color?: any };
+  autoclose = true;
   constructor() {
     super();
   }
@@ -40,35 +43,44 @@ export default class Side extends Scope {
   }
   open(tagOrEl: string | HTMLElement, options?: ISideOptions) {
     this.close();
-    this.size = options?.size||'12em';
-    this.pos = options?.pos || 'r';
+    this.size = options?.size || "12em";
+    this.pos = options?.pos || "r";
+    this.autoclose = (options?.autoclose === false )?false:true;
+    let target = tagOrEl instanceof HTMLElement ? tagOrEl : document.createElement(tagOrEl);
 
     // 下个周期插入元素,
-    requestAnimationFrame(()=>{
-        let target = tagOrEl instanceof HTMLElement ? tagOrEl : document.createElement(tagOrEl);
-        if(options?.attrs){
-          for(let k of Object.keys(options.attrs)){
-            let v = options.attrs[k];
-            target.setAttribute(k,v);
-          }
+    requestAnimationFrame(() => {
+      if (options?.attrs) {
+        for (let k of Object.keys(options.attrs)) {
+          let v = options.attrs[k];
+          target.setAttribute(k, v);
         }
-        this.$id.side.appendChild(target);
-        this.el = target;
-        this.show = true;
-        this.$emit('blur-bg',true)
-    })
-
+      }
+      this.$id.side.appendChild(target);
+      this.el = target;
+      this.show = true;
+      this.$emit("blur-bg", true);
+    });
+    let closeListener = (ev:Event)=>{
+        if(ev.target == target){
+            this.hide();
+            // 移除监听
+            target.removeEventListener('close',closeListener);
+        }
+    }
+    // 监听 close 事件
+    target.addEventListener('close',closeListener)
+    return target;
   }
   hide() {
     this.show = false;
-    this.$emit('blur-bg',false)
+    this.$emit("blur-bg", false);
   }
   close() {
     if (this.el) {
       this.el.remove();
       this.el = undefined;
     }
-    this.show=false;
+    this.show = false;
   }
 }
-
