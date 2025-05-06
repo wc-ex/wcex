aded3f9f20b893292839a902bb27b574a15f9148
zhufeng <zhfjyq@gmail.com>
2025-05-06 11:41:04 +0800

    OK

--- ui/src/dialog.ts
+++ ui/src/dialog.ts
@@ -1,23 +1,26 @@
 // 防止影响，将dialog移动到body中弹出，弹出位置：默认水平居中，垂直为top:30%
 // 背景虚化
 
-import {Scope} from 'wcex'
+import { Logger, Scope } from 'wcex'
+let log = Logger("dialog")
 
 
 export interface IDialogOptions {
 
 }
-
-
+log.info("----------->> INIT Dialog")
 WCEX.usePlugins({
-    name: "$dialog",    
-    scope: {
-        $dialog(dlgEl:HTMLElement, options?: IDialogOptions) {
-      },
+  name: "$dialog",
+  scope: {
+    $dialog(id:string) {
+      log.info("dialog", id)
+      
     },
-  });
+  },
+});
 
-export default class extends Scope{
-    onReady(): void {
-    }
+export default class extends Scope {
+  onReady(): void {
+  }
 }
+
